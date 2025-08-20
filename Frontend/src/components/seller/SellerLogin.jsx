f2fb65b0b9c0820712f9e0f5bffce0e0a2359674
satyam kumar mishra <satyamkmishraa@gmail.com>
2025-08-20 14:15:37 +0530

    First Commit

--- Frontend/src/components/seller/SellerLogin.jsx
+++ Frontend/src/components/seller/SellerLogin.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../../context/AppContext'
 import toast from 'react-hot-toast';
 
-const SellerLogin = () => {
+const Sellerlogin = () => {
     const {isSeller, setIsSeller, navigate, axios} = useAppContext();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -56,4 +56,4 @@ const SellerLogin = () => {
   )
 }
 
-export default SellerLogin
+export default Sellerlogin

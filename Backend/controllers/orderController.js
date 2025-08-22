dc16eef2470bbe51ad9e66ea3a32761819129d27
satyam kumar mishra <satyamkmishraa@gmail.com>
2025-08-22 06:59:39 +0530

    first commit

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -180,8 +180,8 @@ export const getUserOrders = async (req, res) => {
         const userId  = req.userId;
         const orders = await Order.find({
             userId,
-            $or: [{paymentType: "COD"},
-                {isPaid: true}]
+            // $or: [{paymentType: "COD"},
+            //     {isPaid: true}]
             }).populate("items.product address").sort({createdAt: -1});
             res.json({ success: true, orders });
 
@@ -197,8 +197,8 @@ export const getUserOrders = async (req, res) => {
 export const getAllOrders = async (req, res) => {
     try {
         const orders = await Order.find({
-            $or: [{paymentType: "COD"},
-                {isPaid: true}]
+            // $or: [{paymentType: "COD"},
+            //     {isPaid: true}]
             }).populate("items.product address").sort({createdAt: -1});
             res.json({ success: true, orders });
 

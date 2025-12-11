9bb4a0111a7105c79c4792c3dfabb6d44ce2fed0
satyam kumar mishra <satyamkmishraa@gmail.com>
2025-12-11 15:18:00 +0530

    Final Commit

--- Frontend/src/components/Loading.jsx
+++ Frontend/src/components/Loading.jsx
@@ -5,7 +5,7 @@ import { useLocation } from 'react-router-dom';
 const Loading = () => {
 
     const {navigate} = useAppContext();
-    let { search } =useLocation();
+    let { search } = useLocation();
     const query = new URLSearchParams(search);
     const nextUrl = query.get('next');
 
@@ -15,13 +15,12 @@ const Loading = () => {
                 navigate(`/${nextUrl}`)
             }, 5000)
         }
-    })
+    }, [nextUrl])
   return (
     <div className='flex justify-center items-center h-screen'>
         <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-primary'>
 
-        </div>
-      
+        </div>      
     </div>
   )
 }
